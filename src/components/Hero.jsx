c7b23cd93f5d5680be07f8c4598cacbef5f9679a
Abhishekkatale <abhishekkatale427@gmail.com>
2024-10-06 07:28:58 +0530

    background change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { MdArrowDownward } from 'react-icons/md';
 import { Link } from 'react-scroll';
+import heroImage from '../assets/hero1.jpg'; // Import the image
 
 const Arrow = () => (
   <motion.div
@@ -22,11 +23,19 @@ const Hero = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1 }}
-      className="bg-[#040404] text-[#D8DBD5] px-5 h-screen flex items-center justify-center flex-col"
+      className="bg-[#040404] text-[#D8DBD5] px-5 h-screen flex items-center justify-center flex-col bg-cover bg-center relative"
+      style={{ backgroundImage: `url(${heroImage})` }} // Use imported image here
     >
-      <div className="text-center">
-        <h1 className="text-5xl font-bold mb-4">Welcome to <span className='uppercase'>Aspire Sports Academy</span></h1>
-        <p className="text-xl mb-8 text-[#7C7C7C]">Unlock Your Potential, Achieve Your Goals</p>
+      {/* Overlay for better text visibility */}
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      
+      <div className="relative text-center">
+      <h1 className="text-5xl font-bold mb-4">
+  Welcome to <span className="uppercase bg-clip-text text-transparent bg-gradient-to-r from-[#6CD123] to-[#38BDF8]">
+    Aspire Sports
+  </span>
+</h1>
+        <p className="text-2xl mb-8 text-gray-400">Reach New Heights</p>
         <Link
           to="services"
           smooth={true}
@@ -41,7 +50,7 @@ const Hero = () => {
           </motion.button>
         </Link>
       </div>
-        <Arrow />
+      <Arrow />
     </motion.section>
   );
 };

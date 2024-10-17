4021b64410afc9ea66f205234674974f2cb76dfa
Abhishekkatale <abhishekkatale427@gmail.com>
2024-10-17 11:33:43 +0530

    upadte

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-// App.jsx
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 import LandingPage from './pages/LandingPage';
 import TermsAndConditions from './pages/TermsAndConditions.jsx';
 import PolicyPages from './pages/PolicyPages.jsx';
@@ -9,24 +9,23 @@ import Membership from './pages/Membership';
 import Contact from './pages/Contact';
 import About from './pages/About';
 
-
-
-
 function App() {
   return (
-    <Routes>
-      <Route path="/service" element={<Service />} />
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={<LandingPage />} />
-      <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-      <Route path="/privacy-policy" element={<PolicyPages />} />
-      <Route path="/sports" element={<Sports />} />
-      <Route path="/membership" element={<Membership />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/service" element={<Service />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/privacy-policy" element={<PolicyPages />} />
+        <Route path="/sports" element={<Sports />} />
+        <Route path="/membership" element={<Membership />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
